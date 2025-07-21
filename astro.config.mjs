9d8b5a073c324e5fd2f602696bb941702a57d30b
jose <josejpgg1@gmail.com>
2025-07-21 23:08:48 +0200

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,24 +3,29 @@ import { defineConfig, envField } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import node from '@astrojs/node';
 
-import vercel from '@astrojs/vercel';
-
 export default defineConfig({
-  adapter: vercel(),
   output: 'server',
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   devToolbar: {
     enabled: false
   },
+
   env: {
     schema: {
       PUBLIC_SHOW_BUY_BUTTON: envField.boolean({ default: true, context: 'server', access: 'public' }),
       PUBLIC_SCORE_API_ENDPOINT: envField.string({ context: 'server', access: 'public' })
     }
   },
+
   server: {
     host: true,
-  }
+  },
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
