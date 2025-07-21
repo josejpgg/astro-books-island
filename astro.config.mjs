fdf2ebef06e218ce1c31c67cffd11d7282c12bdf
José Jean Pierre Gamarra Gaspar <josejp@Joses-MP.local>
2025-07-21 16:10:43 +0200

    initial static web page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import node from '@astrojs/node';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  adapter: node({
+    mode: 'standalone',
+  }),
+  output: 'server',
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  devToolbar: {
+    enabled: false
+  },
+  env: {
+    schema: {
+      PUBLIC_SHOW_BUY_BUTTON: envField.boolean({ default: true, context: 'server', access: 'public' }),
+      PUBLIC_SCORE_API_ENDPOINT: envField.string({ context: 'server', access: 'public' })
+    }
+  }
+});
